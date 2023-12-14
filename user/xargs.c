556ebc82cab68524694b84d540262b72cbefac0c
venture_kwok <venturekwok@gmail.com>
2023-12-14 14:56:08 -0800

    Pass xargs, left primes

--- user/xargs.c
+++ user/xargs.c
@@ -8,41 +8,7 @@
 #define XARG_FILE_STDOUT 1
 #define XARG_FILE_STDERR 2
 
-int Dup2(int old_fd, int new_fd){
-    if (old_fd == new_fd){
-        return new_fd;
-    }
-
-    // close the new fd if it's open
-    close(new_fd);
-    int temp_fd;
-
-    while ((temp_fd = dup(old_fd)) != new_fd) {
-        if (temp_fd == -1){
-            printf("Dup2 fails\n");
-            exit(1);
-        }
-    }
-
-    return new_fd;
-}
-
-
-int main(int argc, char** argv) {
-    if (argc < 3) {
-        printf("xarg requires at least three args\n");
-        exit(1);
-    }
-
-    char buf[512];
-    int n;
-    printf("being to read\n");
-    while((n = read(XARG_FILE_STDIN, buf, sizeof(buf))) >= 0) {
-        printf("we read: %s\n", buf);
-        exit(0);
-    }
-    printf("end to read\n");
-
+void ExecuteInXargs(char** argv, char* args[MAXARG]) {
     int pipe_fd[2];
     if  (pipe(pipe_fd) == -1) {
         fprintf(2, "error when call pipe\n");
@@ -56,29 +22,45 @@ int main(int argc, char** argv) {
     } else if(pid == 0) {
         // child process
         close(pipe_fd[0]);
-        Dup2(pipe_fd[1], XARG_FILE_STDOUT);
         close(pipe_fd[1]);
-        
-        char* first_part_argv [MAXARG];
-        for (int i = 1 ; i <= argc; i++) {
-            if (i == argc) {
-                first_part_argv[i - 2][0] = '\0';
-            }else {
-                strcpy(first_part_argv[i - 2], argv[i]);
-            }
-        }
 
-        
-        for (int i = 0; strcmp(first_part_argv[i], "\0") != 0 ; i++) {
-            printf("the arg is %s\n", first_part_argv[i]);
-        }
-
-        exec(argv[1], first_part_argv);
-        // should not reach here;
+        exec(argv[1], args);
+        // Should not reach here
         exit(1);
     } else {
         // parent process
+        wait(0);
     }
+}
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        printf("xarg requires at least three args\n");
+        exit(1);
+    }
+
+    char buf[512];
+    char* args[MAXARG];
+    args[0] = argv[1];
+    while (1) {
+        gets(buf, 512);
+        if (buf[0] == '\0') {
+            break;
+        }
+
+        buf[strlen(buf) - 1] = '\0';
+        // printf("what we read: %s\n", buf);
+
+        for (int i = 2; i < argc; i++){
+            args[i - 1] = argv[i];
+        }
+
+        args[argc - 1] = buf;
+        args[argc] = 0;
+
+        ExecuteInXargs(argv, args);
+    }
+
 
     exit(0);
 }
