a0c5820422d88ae2096c8ee857035427917fa23c
venture_kwok <venturekwok@gmail.com>
2024-02-04 18:11:36 -0800

    Pass Barrier

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,23 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+
+  // 3
+  // 1
+  // 2
+  // 3
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  bstate.nthread++;
+
+  if (bstate.nthread == nthread) {
+    bstate.round++;
+    bstate.nthread = 0;
+    pthread_cond_broadcast(&(bstate.barrier_cond));
+  } else {
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  }
+
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
